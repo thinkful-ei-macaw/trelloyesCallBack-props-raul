2e714ef1338552e0ca22f2ae4ed71f9ba5fd261d
Raul Garcia <punkvomit21@gmail.com>
2020-03-06 19:58:53 -0500

    add delete button and css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import List from './List';
 import './App.css';
+import STORE from './STORE';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+function omit(obj, keyToOmit) {
+  return Object.entries(obj).reduce(
+    (newObj, [key, value]) =>
+      key === keyToOmit ? newObj : { ...newObj, [key]: value },
+    {}
   );
+}
+
+function newRandomCard(cardId) {
+  const id = Math.random().toString(36).substring(2, 4)
+    + Math.random().toString(36).substring(2, 4);
+  return {
+    id,
+    title: `Random Card ${id}`,
+    content: 'lorem ipsum'
+  }
+
+}
+
+class App extends Component {
+  state = {
+    store: STORE
+  }
+
+
+
+  handleDeleteCard = (id) => {
+    const newLists = this.state.store.lists.map(function (list) {
+      return {
+        ...list, cardIds: list.cardIds.filter(function (cardId) {
+          return cardId !== id;
+        })
+      };
+    })
+
+
+    const allCards = omit(this.state.store.allCards, id);
+
+
+
+    this.setState({
+      store: {
+        lists: newLists,
+        allCards: allCards
+
+      }
+    })
+  };
+
+
+  addRandomCard = listId => {
+    const myCard = newRandomCard();
+    const newLists = this.state.store.lists.map(list => {
+      if (list.id === listId) {
+        return {
+          ...list,
+          cardIds: [...list.cardIds, myCard.id]
+        };
+      }
+      return list;
+    })
+
+    this.setState({
+      store: {
+        lists: newLists,
+        allCards: {
+          ...this.state.store.allCards,
+          [myCard.id]: myCard
+        }
+      }
+    })
+  };
+
+
+
+
+  render() {
+    const { store } = this.state
+    return (
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+          {store.lists.map(list => (
+            <List
+              key={list.id}
+              id={list.id}
+              header={list.header}
+              cards={list.cardIds.map(id => store.allCards[id])}
+              clickDeleteCard={this.handleDeleteCard}
+              clickRandomCard={this.addRandomCard}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
